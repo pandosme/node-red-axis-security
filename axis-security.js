96b7a7f658c48a7fb4bc50211a67d0d81d493c96
Fred Juhlin <fred@juhlin.me>
2021-03-19 12:55:54 +0100

    Added actions

--- axis-security.js
+++ axis-security.js
@@ -1,3 +1,5 @@
+//Copyright (c) 2021 Fred Juhlin
+
 const VapixWrapper = require('vapix-wrapper');
 
 module.exports = function(RED) {
@@ -41,8 +43,12 @@ module.exports = function(RED) {
 				break;
 
 				case "Set account":
-					//options can be JSON string or object and must include name,password & priviliges
-//					console.log(action, options);
+					if( !options || !(typeof options === "string" || typeof options === "object") ) {
+						msg.error = "Invalid input";
+						msg.payload = "Set account options (JSON or object)";
+						node.send(msg);
+						return;
+					}
 					VapixWrapper.Account_Set( device, options, function(error, response){
 						msg.error = error;
 						msg.payload = response;
@@ -64,6 +70,65 @@ module.exports = function(RED) {
 					});
 				break;
 
+				case "Set common hardening":
+					if( !options || !(typeof options === "string" || typeof options === "object") ) {
+						msg.error = "Invalid input";
+						msg.payload = "Set account options (JSON or object)";
+						node.send(msg);
+						return;
+					}
+					if( typeof options === "string" )
+						options = JSON.parse(options);
+					
+					if( !options || typeof options !== "object" ) {
+						msg.error = "Invalid input";
+						msg.payload = "Set account options (JSON or object)";
+						node.send(msg);
+						return;
+					}
+					
+				
+					node.warn( action + "is not yet implemented");
+				break;
+				
+				case "Enforce HTTPS":
+					node.warn( action + "is not yet implemented");
+				break;
+				
+				case "Set IP whitelist":
+/*				
+				    var ipFormat = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+					var whitelist = "no";
+					var list = "";
+					if( Array.isArray(options) && options.length > 0 ) {
+						if( !ipFormat.test(msg.payload[0]) ) {
+							msg.error = "Invalid input");
+							msg.payload = msg.payload[0] + " is invalid IP address";
+							node.send(msg);
+							return;
+						}
+						whitelist = "yes";
+						list = msg.payload[0];
+						for(var i = 1; i < msg.payload.length;i++) {
+							if( !ipFormat.test(msg.payload[i]) ) {
+								msg.error = "Invalid input");
+								msg.payload = msg.payload[i] + " is invalid IP address";
+								node.send(msg);
+								return;
+							}
+							list += "%20" + msg.payload[i];
+						}
+					}
+					var cgi = "/axis-cgi/param.cgi?action=update&root.Network.Filter.Enabled=" + whitelist;
+					cgi += "&root.Network.Filter.Input.AcceptAddresses=" + list;
+					VapixWrapper.CGI( device, cgi, function(error,response ) {
+						msg.error = error;
+						msg.payload = response;
+						node.send(msg);
+					});
+*/					
+				break;
+
 				case "List certificates":
 					VapixWrapper.Certificates_List( device, function(error, response){
 						msg.error = error;
@@ -83,6 +148,14 @@ module.exports = function(RED) {
 						msg.payload = response;
 						node.send(msg);
 					});
+				break;
+
+				case "Install signed certificate":
+					node.warn( action + "is not yet implemented");
+				break;
+				
+				case "Install P12 certificate":
+					node.warn( action + "is not yet implemented");
 				break;
 				
 				default:
